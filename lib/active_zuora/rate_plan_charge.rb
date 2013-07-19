6906ead8fbef7105193359ebc29452e2f9411b6a
Nate Greene <nate.greene@tstmedia.com>
2013-07-19 13:14:15 -0500

    fix typo

--- lib/active_zuora/rate_plan_charge.rb
+++ lib/active_zuora/rate_plan_charge.rb
@@ -17,7 +17,7 @@ module Zuora
       @product_rate_plan_charge_tier ||= ProductRatePlanChargeTier.where(query).first
     end
 
-    def charge_model_query(identifier_name, identifier_value)
+    def charge_tier_query(identifier_name, identifier_value)
       query = "#{identifier_name} = '#{identifier_value}'"
       query += " and endingUnit >= #{charge_quantity} and startingUnit <= #{charge_quantity}" if chargeModel == "Volume Pricing"
       query
